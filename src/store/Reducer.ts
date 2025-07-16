63a735f45fd5e60efeac4830220f8cefa7fc52ee
bindassnd-glitch <bindassnd@gmail.com>
2025-07-16 14:29:51 +0530

    Delete src directory

--- src/store/Reducer.ts
+++
@@ -1,9 +0,0 @@
-import { initialStateType } from "@/types/Reducer.types";
-
-export const initialState: initialStateType = {} as initialStateType;
-
-export const reducer = (state: initialStateType): initialStateType => {
-    return state;
-}
-
-export default reducer;
--- src/store/StoreContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-import { initialState } from './Reducer';
-import type { StoreContextType } from '../types/Reducer.types';
-
-export const StoreContext = createContext<StoreContextType>({
-    state: initialState,
-    dispatch: () => { },
-});
