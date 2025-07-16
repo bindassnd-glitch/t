320fcf2a9ed687a234aaee721401ebcf9ef47f19
bindassnd-glitch <bindassnd@gmail.com>
2025-07-16 11:34:04 +0530

    Delete src directory

--- src/types/Reducer.types.ts
+++
@@ -1,7 +0,0 @@
-// Template for future global state
-export type initialStateType = unknown;
-export type Action = unknown;
-export interface StoreContextType {
-    state: initialStateType;
-    dispatch: (action: Action) => void;
-}
